116158e0d99dd415b6fdf89e2720cfb965b8ed7f
R Kanishkasree <kanishkasree804@gmail.com>
2024-08-08 10:01:29 +0530

    basic setup

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController')
 const auth = require('../middlewares/auth')
 
-router.get('/products',auth,productController.getAllProducts)
+router.get('/products',productController.getAllProducts)
 router.post('/addProduct',auth,productController.addProduct)
 router.put('/updateProduct/:id',auth,productController.updateProduct)
 router.delete('/deleteProduct/:id',auth,productController.deleteProduct)
